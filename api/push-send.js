6f54a93d503b2bcf8c0c790ad55eb63e27ae6823
aleksandarpopup-afk <aleksandarpopup@gmail.com>
2026-09-18 12:04:13 +0200

    Update push-send.js

--- api/push-send.js
+++ api/push-send.js
@@ -1,7 +1,7 @@
 import webpush from "web-push";
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_KEY = process.env.SUPABASE_ANON_KEY;
+const SUPABASE_KEY = process.env.SUPABASE_SERVICE_KEY;
 const VAPID_PUBLIC = process.env.VAPID_PUBLIC_KEY;
 const VAPID_PRIVATE = process.env.VAPID_PRIVATE_KEY;
 const VAPID_EMAIL = process.env.VAPID_EMAIL || "mailto:test@test.com";
