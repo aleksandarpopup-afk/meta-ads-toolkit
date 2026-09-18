68055336c982b05cabb6aa5d126961ea20a18956
aleksandarpopup-afk <aleksandarpopup@gmail.com>
2026-09-18 12:07:21 +0200

    Update analyses.js

--- api/analyses.js
+++ api/analyses.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_KEY = process.env.SUPABASE_ANON_KEY;
+const SUPABASE_KEY = process.env.SUPABASE_SERVICE_KEY;
 
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
